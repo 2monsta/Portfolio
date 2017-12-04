3fa338275b51134bd3363faf06db8c86f7aad987
Jason Li <Yunf.li37@gmail.com>
2017-12-04 13:30:52 -0500

    update form to work

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,16 +1,6 @@
+
 $(document).ready(()=>{
 	$('[data-toggle="tooltip"]').tooltip();
 	$(".button-collapse").sideNav();
 	$('.parallax').parallax();
-
-	$(".navbar-fixed").hide();
-	$(window).scroll(function () {
-				// set distance user needs to scroll before we start fadeIn
-		if ($(this).scrollTop() > 100) {
-			$('.navbar-fixed').fadeIn(500);
-		} else {
-			$('.navbar-fixed').fadeOut(500);
-		}
-	});
-
 })
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,53 @@
 var express = require('express');
 var router = express.Router();
-
+var nodemailer = require("nodemailer");
+var config = require("../config/config");
+var transport = {
+	host: 'smtp.gmail.com',
+	auth: {
+		user: config.USER,
+		pass: config.pass
+	}
+}
+var transporter = nodemailer.createTransport(transport);
+transporter.verify((error,success)=>{
+	if(error){
+		console.log(error)
+	}else{
+		console.log("server is ready to take messages");
+	}
+})
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+	var message = '';
+	if(req.query.msg != undefined){
+		message = req.query.msg;
+	}
+  res.render('index', {message:message});
 });
 
 router.get("/resume", (req, res, next)=>{
     res.render("resume");
 });
 
-
+router.post("/mail", (req, res, next)=>{
+	var {name, email, phone, message} = req.body;
+	var finalMessage = `${message} \n\n phone: ${phone} \n email: ${email}`;
+	var mail = {
+		from: email,
+		to: 'yunf.li37@gmail.com',
+		subject: name,
+		text: finalMessage
+	}
+	transporter.sendMail(mail, (error, data)=>{
+		if(error){
+			console.log(error);
+			res.redirect("/?msg=Fail");
+		}else{
+			console.log("success");
+			res.redirect("/?msg=Thank%20You%20For%20Contacting%20Me");
+		}
+	});
+})
 
 module.exports = router;
